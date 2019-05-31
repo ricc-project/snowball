b602b7ac82bbfd459ebff95aedea8136b0c77953
pablodiegoss <pablodiegoss@hotmail.com>
2019-05-31 16:17:41 -0300

    Configure routes

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from boogie.rest import rest_api
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('ricc.routes')),
+    path('api/', include(rest_api.urls))
 ]
