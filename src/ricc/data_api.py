91fc2a177ae33ff555bfb42b17ea9927a1933a57
pablodiegoss <pablodiegoss@hotmail.com>
2019-06-02 18:44:07 -0300

    Create routes for create central, station and actuator

--- src/ricc/data_api.py
+++ src/ricc/data_api.py
@@ -1,19 +1,16 @@
 import json
 import requests
 
-def create_user_on_data_api(user, username, password):
+def create_user_on_data_api(username, password):
     data = {
         'username':username,
         'password':password
     }
-    
     result = request_data_api('signup/', data)
     result = json.loads(result)
-    user.data_token = result['authentication_token']
-    user.save()
 
-    return result
-
+    return result['authentication_token']
+    
 def request_data_api(end_point, data={}):
     post_url = "http://localhost:8080/" + end_point
 
--- src/ricc/routes.py
+++ src/ricc/routes.py
@@ -1,9 +1,9 @@
-import hashlib, binascii, requests, json
+import hashlib, binascii, requests, json, functools
 from rest_framework import status
 from boogie.router import Router
 from django.db import models
 from django.http import HttpResponse
-from .models import User
+from .models import User, Station, Central, Actuator
 from .manager import verify_password
 from .data_api import create_user_on_data_api
 
@@ -14,9 +14,10 @@ urlpatterns = Router()
 def signup(request):
     rstatus = status.HTTP_403_FORBIDDEN
     if(request.method == "POST"):
-        if request.POST.keys() >= {"username", "password"}:
-            username = request.POST['username']
-            password = request.POST['password']
+        data = json.loads(request.body)
+        if data.keys() >= {"username", "password"}:
+            username = data['username']
+            password = data['password']
             # try:
             user = User.objects.create_user(username, password)
             response = {"authentication_token": user.auth_token}
@@ -64,10 +65,67 @@ def verify_sent_credentials(request):
     return False
 
 
+@urlpatterns.route("create_central/")
+def create_central(request):
+    user = verify_auth(request)
+    if(user):
+        mac_address = json.loads(request.body)['mac_address']
+        Central.objects.create_central(user, mac_address)
+        return HttpResponse("beautiful", status=status.HTTP_201_CREATED)
+    else:
+        return HttpResponse("Unauthorized.", status=status.HTTP_401_UNAUTHORIZED)
 
 @urlpatterns.route("create_station/")
 def create_station(request):
-    pass
-    # user = create_user_on_data_api(user, username, password)
-            
+    user = verify_auth(request)
+    if(user):
+        name = json.loads(request.body)['name']
+        central = get_central(request)
+        if(central):
+            node_authentication = create_user_on_data_api(name, user.auth_token)
+            Station.objects.create_station(name, central, node_authentication)
+            return HttpResponse("beautiful", status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse("No central informed", status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return HttpResponse("Unauthorized.", status=status.HTTP_401_UNAUTHORIZED)
 
+@urlpatterns.route("create_actuator/")
+def create_actuator(request):
+    user = verify_auth(request)
+    if(user):
+        name = json.loads(request.body)['name']
+        central = get_central(request)
+        if(central):
+            node_authentication = create_user_on_data_api(name, user.auth_token)
+            Actuator.objects.create_actuator(name, central, node_authentication)
+            return HttpResponse("beautiful", status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse("No central informed", status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return HttpResponse("Unauthorized.", status=status.HTTP_401_UNAUTHORIZED)
+
+
+def verify_auth(request):
+    if(request.method == "POST"):
+        data = json.loads(request.body)
+        if 'auth_token' in data.keys():
+            user = User.objects.filter(auth_token=data['auth_token'])
+            print(user)
+            if user:
+                return user.first()
+        return None
+    else:
+        return None
+
+def get_central(request):
+    if(request.method == "POST"):
+        data = json.loads(request.body)
+        if 'central' in data.keys():
+            central = Central.objects.filter(mac_address=data['central'])
+            print(central)
+            if central:
+                return central.first()
+        return None
+    else:
+        return None
