24d5c4bd48411da721790835e29a95d891c0923b
pablodiegoss <pablodiegoss@hotmail.com>
2019-06-17 15:57:12 -0300

    Fix login

--- src/ricc/routes.py
+++ src/ricc/routes.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from boogie.router import Router
 from django.db import models
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import User, Station, Central, Actuator, UnlockedCentral
 from .manager import verify_password
 from .data_api import create_user_on_data_api, send_data
@@ -18,9 +19,8 @@ def signup(request):
         if data.keys() >= {"username", "password"}:
             username = data['username']
             password = data['password']
-            
             user = User.objects.create_user(username, password)
-            if(model):
+            if(user):
                 response = {"authentication_token": user.auth_token}
                 rstatus = status.HTTP_201_CREATED
             else:
@@ -37,34 +37,29 @@ def signup(request):
 @urlpatterns.route("login/")
 def login(request):
     rstatus = status.HTTP_403_FORBIDDEN
-    if(verify_sent_credentials(request)):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = None
-        try:
-            user = User.objects.get(username=username)
-        except:
-            response = response = {"Incorrect Credentials": "Invalid login information."}
+    if(request.method == "POST"):
+        data = json.loads(request.body)
+        if data.keys() >= {"username", "password"}:
+            data = json.loads(request.body)
+            username = data['username']
+            password = data['password']
+            user = None
+            try:
+                user = User.objects.get(username=username)
+            except:
+                response = response = {"Incorrect Credentials": "Invalid login information."}
 
-        if(verify_password(user, password)):
-            response = {"authentication_token": user.auth_token}
-            rstatus = status.HTTP_202_ACCEPTED
-        else:
-            response = {"Incorrect Credentials": "Invalid login information."}
+            if(verify_password(user, password)):
+                response = {"authentication_token": user.auth_token}
+                rstatus = status.HTTP_202_ACCEPTED
+            else:
+                response = {"Incorrect Credentials": "Invalid login information."}
     else:
         response = {
             "Not enough information sent to do this.":
             "No username or Password, maybe not a POST method."}
 
     return HttpResponse(str(response), status=rstatus)
-
-
-def verify_sent_credentials(request):
-    if(request.method == "POST"):
-        if request.POST.keys() >= {"username", "password"}:
-            return True
-    return False
-
 
 @urlpatterns.route("create_central/")
 def create_central(request):
